0147ec0021a3e9b4199b0b7e0cc5ecb5ddb8f5ad
twofish0808 <68212690+twofish0808@users.noreply.github.com>
2023-06-16 00:12:27 +0800

    big big big update

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,77 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+var images = [
+    "/assets/img/index1.jpg",
+    "/assets/img/index2.jpg",
+    "/assets/img/index3.jpg"
+  ];
+  var currentIndex = 0;
+  var masthead = document.querySelector('header.masthead');
+  
+  function changeBackground() {
+    masthead.style.backgroundImage = 'url(' + images[currentIndex] + ')';
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+  
+  // Initially set the background image
+  changeBackground();
+  
+  // Change the background image every 10 seconds
+  setInterval(changeBackground, 10000);
+
+
+
+  // Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD_INROKwLo8GY9wqiyHORRqfnP5k8eBwY",
+    authDomain: "frontend-99dda.firebaseapp.com",
+    projectId: "frontend-99dda",
+    storageBucket: "frontend-99dda.appspot.com",
+    messagingSenderId: "744993888625",
+    appId: "1:744993888625:web:c877701ee42e803af03ed0"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  var contactForm = document.getElementById('contactForm');
+  contactForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+  
+    var formData = {
+      name: name,
+      email: email,
+      phone: phone,
+      message: message
+    };
+  
+    // Get a reference to the Firestore service
+    var db = firebase.firestore();
+  
+    // Push the form data to your Firebase Firestore Database
+    db.collection('contactsTEST').add(formData)
+    .then(() => {
+      // Data saved successfully!
+      successAlert.classList.remove('d-none');
+      errorAlert.classList.add('d-none');
+    })
+    .catch((error) => {
+      // The write failed...
+      successAlert.classList.add('d-none');
+      errorAlert.classList.remove('d-none');
+    });
+  });
+  
+  
+
+
+  
+
+  
